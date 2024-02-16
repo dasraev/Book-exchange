f0b4be1ec427793694b912b0c57589b1edbccf1b
Asrayev Dilshod <dasraev@Asrayevs-MacBook-Air.local>
2024-02-16 10:13:34 +0500

    almost nothing

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,8 @@
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
 from .models import Room,Message
 from django.contrib.auth import get_user_model
-from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 User = get_user_model()
 

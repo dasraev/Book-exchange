8835daa51351d39a9d4a12eaa40b83144e27d5cd
Asrayev Dilshod <dasraev@Asrayevs-MacBook-Air.local>
2024-02-16 10:15:30 +0500

    almost nothing2

--- BookEx/asgi.py
+++ BookEx/asgi.py
@@ -7,15 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 import os
-from chat import routing
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookEx.settings')
+
 from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from chat import routing
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookEx.settings')
 
 
 application = ProtocolTypeRouter({
